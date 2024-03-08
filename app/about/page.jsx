1231ea8e04e36c0c4128723faec66a3fd3fc1b31
tupudipi <tupudipi@gmail.com>
2024-03-08 13:02:36 +0200

    added an 'about' page. modified footer to be responsive

---
+++ app/about/page.jsx
@@ -0,0 +1,32 @@
+
+const AboutPage = () => {
+    return (
+        <div className="grow mt-8">
+            <h1 className="text-xl font-semibold mb-6">Meow-velous Greetings, Admirers of the Extraordinary!</h1>
+            <p className="text-justify">
+                I am Cico, the illustrious feline behind this dazzling corner of the internet. This blog is my canvas, where I paw-sitively dazzle you with tales of my exquisite taste in fish pâté, masterful laser pointer acrobatics, and the art of morning cuddles. You may wonder, &apos;Why does a cat need a blog?&apos; Well, dear friends, I graciously allow my human to share the wonders of my world because, let&apos;s face it, my paws are too busy with napping and bird-watching to manage this digital masterpiece. My devoted owner assists in translating my feline brilliance into words because, alas, I lack the gift of opposable thumbs.
+                <br /><br />
+                Now, let me whisker you away into the enchanting universe of my indoor escapades and occasional outdoor forays. Enjoy the tales, marvel at the photos, and be mesmerized by my sheer charisma.
+                <br /><br />
+                Purr-sistently Yours,
+                <br />
+                Cico 🐾✨
+            </p>
+            <hr />
+            <br />
+
+            <h1 className="text-lg font-semibold mb-4">Editor&apos;s Notes</h1>
+            <p className="text-justify">
+                Welcome to the behind-the-scenes scoop of Cico&apos;s digital lair. Our tech stack is as sleek as Cico&apos;s midnight-blue fur:
+                <br /><br />
+                Here at Cico&apos;s Domain, we take web development as seriously as a fresh bowl of fish pâté. This blog is a charming creation brought to life using <strong>Next.js</strong>, a delightful React framework that ensures seamless navigation through Cico&apos;s anecdotes. The database wizardry is powered by <strong>MongoDB</strong>, orchestrated with precision through <strong>Prisma ORM</strong>.
+                <br /><br />
+                For the purr-fect file hosting, we turn to <strong>Firebase</strong>, ensuring every snapshot of Cico&apos;s captivating moments finds its cozy spot on the web. <strong>Google Auth</strong> extends a friendly paw, allowing users to partake in this feline adventure with secure accounts. And, of course, our sleek design is crafted with love and a dash of catnip, employing <strong>Tailwind CSS</strong>.
+                <br /><br />
+                Wishing You Whiskers and Wonder
+            </p>
+        </div>
+    )
+}
+
+export default AboutPage;
--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -6,14 +6,14 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
     return (
-        <div className="flex justify-between mt-10 mb-2">
-            <div className="w-1/2">
+        <div className="flex flex-col md:flex-row justify-between mt-10 mb-2">
+            <div className="w-full md:w-1/2">
                 <div className="flex gap-4 items-center">
                     <Image src="/logo.png" alt="Cico Blog Logo" width={50} height={50} className="rounded-full" />
                     <h2>Cico Blog</h2>
                 </div>
                 <p>Paws and whiskers, thanks for wandering through my digital realm. Stay curious, stay cozy! 🐾✨</p>
-                <div className="flex gap-4 items-center">
+                <div className="flex gap-4 items-center justify-evenly md:justify-start">
                     <Link href="mailto:tupudipi@gmail.com">
                         <i className="mx-3 text-gray-600 hover:text-black hover:shadow-md transition-all">
                             <FontAwesomeIcon icon={faEnvelope} />
@@ -32,7 +32,7 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className="flex gap-6 w-1/2 justify-around">
+            <div className="flex gap-6 w-full md:w-1/2 justify-around">
                 <div className="flex flex-col gap-2">
                     <span className="text-lg font-semibold mb-2">Links</span>
                     <Link href="/" className="hover:text-blue-500 transition-all">Homepage</Link>
