3f608f34339acf462d69c3bc5ac2ecc05a8093af
tupudipi <tupudipi@gmail.com>
2024-03-19 12:44:54 +0200

    added mongo database and prisma orm -  adding users to db when registering or logging in

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,10 @@
+import prisma from '@/app/utils/connect';
+import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
@@ -15,7 +18,8 @@ export const authOptions = {
             else if (new URL(url).origin === baseUrl) return url;
             return baseUrl;
         }
-    }
+    },
+    debug: process.env.NODE_ENV === 'development'
 };
 
 export const handler = NextAuth(authOptions);
